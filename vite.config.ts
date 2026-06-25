ff853dfb92cc60f488fa3d5b3e8b9e46b29ca4b5
Jeff Griffiths <jeff@eko-recordings.ca>
2026-06-25 10:13:08 -0700

    added dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
+import 'dotenv/config'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [svelte(), tailwindcss()],
-	base: '/{repo-name}/',
+	base: process.env.MODE === 'production' ? `/${process.env.PAGES_BASE}/` : '',
 })
